1cb46f62a6945573f2e4b2801c33790f41735b5a
shilpashetty <shilpashetty0608@gmail.com>
2022-04-05 13:16:28 +0100

    git commit

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,6 +1,6 @@
 package com.learning.java;
 class Fibonacci {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         int n1=0, n2=1, n3, i, count=10;
         System.out.println(n1+""+ n2);
         for (i = 2; i<count; ++i)
